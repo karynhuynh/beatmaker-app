bc6bc05d2648444fed0cee5c5284e8a92373e041
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-08-05 20:31:28 -0700

    Plays the audio track when the pad has an active class

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ class DrumKit {
   constructor() {
     this.pads = document.querySelectorAll(".pad");
     this.playBtn = document.querySelector(".play");
-    this.kickAudio = document.querySelectorAll(".kick-sound");
-    this.snareAudio = document.querySelectorAll(".snare-sound");
-    this.hihatAudio = document.querySelectorAll(".hihat-sound");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     // beats per minute(bpm)
     this.bpm = 150;
@@ -18,6 +18,23 @@ class DrumKit {
     // loop over the pads and create animation
     activeBars.forEach((bar) => {
       bar.style.animation = "playTrack 0.3s alternate ease-in-out 2";
+      // check if pads are active
+      if (bar.classList.contains("active")) {
+        // check each sound
+        if (bar.classList.contains("kick-pad")) {
+          //resets the time of the audio track
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
     });
     this.index++;
   }
